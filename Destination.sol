89bb85b9f0fe212428ef7d2150d16d784dc9ab16
VanessaLyon <62832343+VanessaLyon@users.noreply.github.com>
2024-02-11 11:36:27 -0500

    Update Destination.sol
    Latest updates

--- Destination.sol
+++ Destination.sol
@@ -27,7 +27,7 @@ contract Destination is AccessControl {
 
 	function createToken(address _underlying_token, string memory name, string memory symbol ) public onlyRole(CREATOR_ROLE) returns(address) {
     
-    require(wrapped_tokens[_underlying_token] == address(0), "Check as per TA guideline"); //still not sure what this does nor why we should do this
+    require(wrapped_tokens[_underlying_token] == address(0), "Check as per TA guideline"); //still no idea what this does nor why we should do this
 
     BridgeToken newBridgeToken = new BridgeToken(_underlying_token, name, symbol, address(this)); // this address
     address newBridgeTokenAddress = address(newBridgeToken);
@@ -54,7 +54,7 @@ contract Destination is AccessControl {
 
   function unwrap(address _wrapped_token, address _recipient, uint256 _amount) public {
     // Ensure the wrapped token address corresponds to a registered BridgeToken contract
-    require(underlying_tokens[_wrapped_token] != address(0), "Wrapped token not registered"); /// changed underlyong[]
+    require(underlying_tokens[_wrapped_token] != address(0), "Wrapped token not registered"); /// changed to underlying[]
 
     // Create an instance of the BridgeToken to interact with
     BridgeToken wrappedToken = BridgeToken(_wrapped_token);
